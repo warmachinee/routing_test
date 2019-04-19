eb127d2a1b6ba0f9b9ee688f1018f497df30b584
P.R.E.M <31282752+warmachinee@users.noreply.github.com>
2019-04-19 20:25:56 +0700

    Test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Home = () =>(
+  <div>Home</div>
+)
+const Main = () =>(
+  <div>Main</div>
+)
+const Dashboard = () =>(
+  <div>Dashboard</div>
+)
+const Profile = () =>(
+  <div>Profile</div>
+)
+
+const Nav = () =>{
+  const item = {
+    margin: '0 8px',
+    textDecoration: 'none'
+  }
+  return(
+    <nav style={{
+        border: '1px solid black',
+        padding: 24,
+        width: '90%'
+      }}>
+      <Link style={item} to="/">Home</Link>
+      <Link style={item} to="/Main">Main</Link>
+      <Link style={item} to="/Dashboard">Dashboard</Link>
+      <Link style={item} to="/Profile">Profile</Link>
+    </nav>
+  );
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+
+        <Nav />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/Main" component={Main} />
+        <Route path="/Dashboard" component={Dashboard} />
+        <Route path="/Profile" component={Profile} />
+      </Router>
     );
   }
 }
